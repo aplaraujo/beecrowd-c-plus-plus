c08b136a3c0d557edf18931ce1f43531c6a57498
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2024-09-01 11:22:15 -0300

    1016

---
+++ exercicios/1016.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int dist, tempo, dif;
+
+    cin >> dist;
+    tempo = 2;
+    dif = dist * tempo;
+    cout << dif << " minutos" << endl;
+    
+    return 0;
+}
